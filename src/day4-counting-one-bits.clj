3dab010de5448e1c2212cfdf582f7506b608fecd
Ayla Khan <ayla.khan@gmail.com>
2020-02-11 22:39:26 -0700

    Counting 1 bits implementation.

---
+++ src/day4-counting-one-bits.clj
@@ -0,0 +1,20 @@
+(ns day4-counting-one-bits)
+
+(defn count-one-bits [value]
+  (loop [value value
+         count-ones 0]
+    (if (zero? value)
+      count-ones
+      (recur
+        (->> value dec (bit-and value))
+        (inc count-ones)))))
+
+(comment
+  (count-one-bits 3)
+  (count-one-bits 2r11)
+  (count-one-bits 99)
+  (count-one-bits 2r1100011)
+  (count-one-bits 2r11001100)
+  (count-one-bits 2r11101101)
+
+  )
